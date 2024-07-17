8d77fdd360886e03cfed662ad89e32c214ba4ec1
salmxnz <138376829+salmxnz@users.noreply.github.com>
2024-07-17 23:24:48 +0400

    camera Half
    Halfway implemented camera app

--- lib/camera/camera_screen.dart
+++ lib/camera/camera_screen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:planty_app/scan_controller.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
 
-class CameraScreen extends StatelessWidget {
+class CameraScreen extends GetView<ScanController> {
   const CameraScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return GetX<ScanController>(builder: (controller) {
+      if (!controller.isInitialized) {
+        return Container();
+      }
+      return CameraPreview(controller.cameraController);
+    });
   }
 }
--- lib/scan_controller.dart
+++ lib/scan_controller.dart
@@ -1,4 +1,38 @@
-import 'package:flutter/material.dart';
-import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:camera/camera.dart';
+import 'package:get/state_manager.dart';
 
-class ScanController extends GetxController {}
+class ScanController extends GetxController {
+  final RxBool _isInitialized = RxBool(false);
+  late CameraController _cameraController;
+  late List<CameraDescription> _cameras;
+
+  bool get isInitialized => _isInitialized.value;
+  CameraController get cameraController => _cameraController;
+
+  Future<void> _initCamera() async {
+    _cameras = await availableCameras();
+    _cameraController =
+        CameraController(_cameras[0], ResolutionPreset.max);
+    _cameraController
+        .initialize()
+        .then((_) {})
+        .catchError((Object e) {
+      if (e is CameraException) {
+        switch (e.code) {
+          case 'CameraAccessDenied':
+            // Handle access errors here.
+            break;
+          default:
+            // Handle other errors here.
+            break;
+        }
+      }
+    });
+  }
+
+  @override
+  void onInit() {
+    _initCamera();
+    super.onInit();
+  }
+}
